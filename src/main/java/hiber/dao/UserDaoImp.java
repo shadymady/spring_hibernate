668910934479cedbceead1df9c37fe79cceaf6fc
Ruslan <r.aleroev@gmail.com>
2021-02-15 18:19:47 +0300

    1

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,40 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user, Car car) {
+        user.setCar(car);
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers(String model, int series) {
+        String hql = "FROM User user WHERE user.car.model = :model AND user.car.series = :series";
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+        return query.getResultList();
+    }
+}
---
+++ src/main/java/hiber/service/UserService.java
@@ -0,0 +1,12 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user, Car car);
+    List<User> listUsers();
+    List<User> listUsers(String model, int series);
+}
